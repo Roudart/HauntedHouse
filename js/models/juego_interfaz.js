2adb2ec206824afd8735733c87856983c50420b8
erubidm <60367052+erubidm@users.noreply.github.com>
2021-11-25 23:56:21 -0600

    nuevos

--- js/models/juego_interfaz.js
+++ js/models/juego_interfaz.js
@@ -1,25 +1,33 @@
-// Get the modal
-var modal = document.getElementById("Score");
 
-// Get the button that opens the modal
-var btn = document.getElementById("openModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+function OpenModal(ModalName, scores){
+  // Get the modal
+  var modal = document.getElementById(ModalName);
   modal.style.display = "block";
-}
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  var scoreList = document.getElementById("ListaScore");
+  for (let i = 0; i < scores.length; i++) {
+    var aux = document.createElement("LI");
+    aux.className = "list-group-item list-group-item-primary";
+    var nombre = scores[i].NombreUsuario
+    var score = scores[i].TopScore
+    aux.innerHTML = nombre + " - " + score;
+    scoreList.appendChild(aux);
+  }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
   }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 }
